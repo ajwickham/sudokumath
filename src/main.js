a83b550970836d1f6a1b6e27229b3e29147eaa1a
Alan Wickham <ajWickham@yahoo.com>
2021-06-03 16:47:11 +0100

    started testing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-export default function SudArray(inputNumber) {
-  this.inputNumber = inputNumber;
+//import $ from 'jquery';
+import 'bootstrap';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+export default function SudArray(first,second,third,fourth,fifth,sixth,seventh,eighth,nineth) {
+  this.first = first;
+  this.second = second;
+  this.third = third;
+  this.fourth = fourth;
+  this.fifth = fifth;
+  this.sixth = sixth;
+  this.seventh = seventh;
+  this.eighth = eighth;
+  this.nineth = nineth;
 }
 
 SudArray.prototype.checkInput = function() {
