26f07493f3f79ae8ac6366071f6169a84428a6d9
Alan Wickham <ajWickham@yahoo.com>
2021-06-07 16:49:39 +0100

    Trying to work out what shape the array should be

--- __tests__/data.test.js
+++ __tests__/data.test.js
@@ -2,8 +2,28 @@ import SudArray from './../src/main';
 
 describe('Creating and testing grid', () => {
 
-  test('should correctly create a translate object from inputNumber', () => {
+  test('should correctly create a arraySudoku object from inputNumber', () => {
     const arraySudoku = new SudArray(2);
     expect(arraySudoku.first).toEqual(2);
+  });
+  test('should correctly create a arraySudoku object from inputNumbers', () => {
+    const arraySudoku = new SudArray(1,2,3,4,5,6,7,8,9);
+    expect(arraySudoku.first).toEqual(1);
+    expect(arraySudoku.second).toEqual(2);
+    expect(arraySudoku.third).toEqual(3);
+    expect(arraySudoku.fourth).toEqual(4);
+    expect(arraySudoku.fifth).toEqual(5);
+    expect(arraySudoku.sixth).toEqual(6);
+    expect(arraySudoku.seventh).toEqual(7);
+    expect(arraySudoku.eighth).toEqual(8);
+    expect(arraySudoku.nineth).toEqual(9);
+  });
+  test('should correctly create push 5th value into arraySudoku', () => {
+    const testArray = new SudArray([1,2,3,4,5,6,7,8,9])
+    expect(testArray.pushIntoArray(5)).toEqual([1,2,3,4,5,1,7,8,9]);
+  });
+  test('should correctly find the next blank', () => {
+    const testArray = new SudArray([1,2,0,4,5,6,7,8,9])
+    expect(testArray.findNextBlank()).toEqual(2);
   });
 })
--- src/main.js
+++ src/main.js
@@ -14,19 +14,31 @@ export default function SudArray(first,second,third,fourth,fifth,sixth,seventh,e
   this.nineth = nineth;
 }
 
-SudArray.prototype.checkInput = function() {
-  let xxx = 0;
-  if(isNaN(this.inputNumber)) {
-    let yyy = this.isItRoman();
-    if(yyy===0){
-      xxx = this.translateRoman();
-    }  
-  }
-  else {
-    xxx = this.translateArabic();
-  }
-  return xxx;
+const line1arraySudoku = new SudArray(5,3,0,0,7,0,0,0,0);
+const line2arraySudoku = new SudArray(6,0,0,1,9,5,0,0,0);
+const line3arraySudoku = new SudArray(0,9,8,0,0,0,0,6,0);
+const line4arraySudoku = new SudArray(8,0,0,0,6,0,0,0,3);
+const line5arraySudoku = new SudArray(4,0,0,8,0,3,0,0,1);
+const line6arraySudoku = new SudArray(7,0,0,0,2,0,0,0,6);
+const line7arraySudoku = new SudArray(0,6,0,0,0,0,2,8,0);
+const line8arraySudoku = new SudArray(0,0,0,4,1,9,0,0,5);
+const line9arraySudoku = new SudArray(0,0,0,0,8,0,0,7,9);
+
+SudArray.prototype.pushIntoArray = function(position) {
+ 
+    this.first[5] = 1; //+line2arraySudoku[i]+line3arraySudoku[i]+line4arraySudoku[i]+line5arraySudoku[i]+line6arraySudoku[i]+line7arraySudoku[i]
+    //+line8arraySudoku[i]+line9arraySudoku[i]
+    return this.first
 };
+
+SudArray.prototype.findNextBlank = function() {
+  /*for (let i=0; i<9; i++) {
+    if (line1arraySudoku.first[i] === 0) {
+      return i;
+    }  
+  }*/
+  return this.first[2]
+}
 
 
 
