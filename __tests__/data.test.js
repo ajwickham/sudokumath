e22b1d6966b0c146b2a5506e15444215f4ed353a
Alan Wickham <ajWickham@yahoo.com>
2021-06-09 16:59:38 +0100

    Working on finding number in row

--- __tests__/data.test.js
+++ __tests__/data.test.js
@@ -4,26 +4,22 @@ describe('Creating and testing grid', () => {
 
   test('should correctly create a arraySudoku object from inputNumber', () => {
     const arraySudoku = new SudArray(2);
-    expect(arraySudoku.first).toEqual(2);
+    expect(arraySudoku.grid).toEqual(2);
   });
   test('should correctly create a arraySudoku object from inputNumbers', () => {
-    const arraySudoku = new SudArray(1,2,3,4,5,6,7,8,9);
-    expect(arraySudoku.first).toEqual(1);
-    expect(arraySudoku.second).toEqual(2);
-    expect(arraySudoku.third).toEqual(3);
-    expect(arraySudoku.fourth).toEqual(4);
-    expect(arraySudoku.fifth).toEqual(5);
-    expect(arraySudoku.sixth).toEqual(6);
-    expect(arraySudoku.seventh).toEqual(7);
-    expect(arraySudoku.eighth).toEqual(8);
-    expect(arraySudoku.nineth).toEqual(9);
+    const arraySudoku = new SudArray([1,2,3,4,5,6,7,8,9]);
+    expect(arraySudoku.grid).toEqual[1,2,3,4,5,6,7,8,9];
   });
-  test('should correctly create push 5th value into arraySudoku', () => {
-    const testArray = new SudArray([1,2,3,4,5,6,7,8,9])
-    expect(testArray.pushIntoArray(5)).toEqual([1,2,3,4,5,1,7,8,9]);
+  test('should correctly find all the blanks', () => {
+    const testArray = new SudArray([1,2,0,4,0,6,7,8,9,0])
+    expect(testArray.findNextBlank()).toEqual([2,4,9]);
   });
-  test('should correctly find the next blank', () => {
-    const testArray = new SudArray([1,2,0,4,5,6,7,8,9])
-    expect(testArray.findNextBlank()).toEqual(2);
+  test('should identify the position of the first number to try', () => {
+    const testArray = new SudArray([1,2,0,4,5,6,7,8,9,1])
+    expect(testArray.findNumberToTry()).toEqual([2]);
+  });
+  test('should identify the lowest missing number missing from a row', () => {
+    const testArray = new SudArray([9,2,1,4,5,6,7,4])
+    expect(testArray.checkRow(2)).toEqual(3);
   });
 })
--- src/main.js
+++ src/main.js
@@ -2,42 +2,51 @@
 import 'bootstrap';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-export default function SudArray(first,second,third,fourth,fifth,sixth,seventh,eighth,nineth) {
-  this.first = first;
-  this.second = second;
-  this.third = third;
-  this.fourth = fourth;
-  this.fifth = fifth;
-  this.sixth = sixth;
-  this.seventh = seventh;
-  this.eighth = eighth;
-  this.nineth = nineth;
+export default function SudArray(grid) {
+  this.grid = grid;
 }
 
-const line1arraySudoku = new SudArray(5,3,0,0,7,0,0,0,0);
-const line2arraySudoku = new SudArray(6,0,0,1,9,5,0,0,0);
-const line3arraySudoku = new SudArray(0,9,8,0,0,0,0,6,0);
-const line4arraySudoku = new SudArray(8,0,0,0,6,0,0,0,3);
-const line5arraySudoku = new SudArray(4,0,0,8,0,3,0,0,1);
-const line6arraySudoku = new SudArray(7,0,0,0,2,0,0,0,6);
-const line7arraySudoku = new SudArray(0,6,0,0,0,0,2,8,0);
-const line8arraySudoku = new SudArray(0,0,0,4,1,9,0,0,5);
-const line9arraySudoku = new SudArray(0,0,0,0,8,0,0,7,9);
+const fullArraySudoku = new SudArray([5,3,0,0,7,0,0,0,0,6,0,0,1,9,5,0,0,0,0,9,8,0,0,0,0,6,0,8,0,0,0,6,0,0,0,3,4,0,0,8,0,3,0,0,1,7,0,0,0,2,0,0,0,6,
+  0,6,0,0,0,0,2,8,0,0,0,0,4,1,9,0,0,5,0,0,0,0,8,0,0,7,9]);
 
 SudArray.prototype.pushIntoArray = function(position) {
- 
-    this.first[5] = 1; //+line2arraySudoku[i]+line3arraySudoku[i]+line4arraySudoku[i]+line5arraySudoku[i]+line6arraySudoku[i]+line7arraySudoku[i]
-    //+line8arraySudoku[i]+line9arraySudoku[i]
-    return this.first
+    this.grid[5] = 1; 
 };
 
 SudArray.prototype.findNextBlank = function() {
-  /*for (let i=0; i<9; i++) {
-    if (line1arraySudoku.first[i] === 0) {
-      return i;
-    }  
-  }*/
-  return this.first[2]
+  let blankPosition = [] 
+  for (let i=0; i<81; i++) {
+    if (this.grid[i] === 0) {
+      blankPosition.push(i);
+    };
+  };
+  return blankPosition
+};
+
+SudArray.prototype.findNumberToTry = function() {
+  let blankPosition = this.findNextBlank();
+  let numberTrial = [];
+  for(let i = 0; i<blankPosition; i++) {
+    //this.checkRow(i);
+  }
+  return blankPosition;
+};
+
+SudArray.prototype.checkRow = function(position) {
+  //let testrow = []
+  let count = 0
+  for (let j = 1; j<10; j++){
+    for (let i=(parseInt(position/9))*9; i<((parseInt(position/9))*9)+9; i++) {
+    //testrow.push(i);
+      if (this.grid[i] === j) {
+        count = 1;
+        }
+      }
+    if (count === 0) {
+      return j  
+    }
+  };
+  //let rowMinimum = Math.min(...testrow);
 }
 
 
