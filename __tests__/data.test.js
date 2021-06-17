ab2ac4bb8c51fdd1d060c2c18060f05757e1b83d
Alan Wickham <ajwickham@yahoo.com>
2021-06-17 17:06:52 +0100

    Able to find an appropriate number for the row and column.  Building ability to capture multiple numbers and their positions

--- __tests__/data.test.js
+++ __tests__/data.test.js
@@ -1,6 +1,6 @@
 import SudArray from './../src/main';
 
-describe('Creating and testing grid', () => {
+describe('Creatingcd and testing grid', () => {
 
   test('should correctly create a arraySudoku object from inputNumber', () => {
     const arraySudoku = new SudArray(2);
@@ -12,14 +12,30 @@ describe('Creating and testing grid', () => {
   });
   test('should correctly find all the blanks', () => {
     const testArray = new SudArray([1,2,0,4,0,6,7,8,9,0])
-    expect(testArray.findNextBlank()).toEqual([2,4,9]);
+    expect(testArray.findBlanks()).toEqual([2,4,9]);
   });
-  test('should identify the position of the first number to try', () => {
-    const testArray = new SudArray([1,2,0,4,5,6,7,8,9,1])
-    expect(testArray.findNumberToTry()).toEqual([2]);
+ test('should identify the lowest missing number missing from a row', () => {
+    const testArray = new SudArray([9,0,1,4,5,6,7,4])
+    expect(testArray.checkRow(2,1)).toEqual(2);
   });
-  test('should identify the lowest missing number missing from a row', () => {
-    const testArray = new SudArray([9,2,1,4,5,6,7,4])
-    expect(testArray.checkRow(2)).toEqual(3);
+  test('should identify the lowest missing number missing from a column', () => {
+    const testArray = new SudArray([1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,3,3,4,3,3,])
+    expect(testArray.checkColumn(2,1)).toEqual(3);
+  });
+  test('should find the blank and check for the lowest missing number in that row. It should then check the column and return the next lowest number in the column', () => {
+    const testArray = new SudArray([1,2,0,5,5,6,7,8,9,1,2,3,4,5,6,7,8,9])
+    expect(testArray.findNumberToTry()).toEqual([[2,4]]);
+  });
+  test('should find the blank and check for the lowest missing number in that row. It should then check the column and return the next lowest number in the column. It should check that number in the row and then in the column until it finds an answer', () => {
+    const testArray = new SudArray([1,2,0,4,7,7,7,8,9,1,2,3,4,5,6,7,8,9,1,2,5,4,5,6,7,8,9])
+    expect(testArray.findNumberToTry()).toEqual([[2,6]]);
+  });
+  test('should correctly create a arraySudoku object from inputNumber', () => {
+    const arraySudoku = new SudArray(2,[0,1]);
+    expect(arraySudoku.solution).toEqual([0,1]);
+  });
+  test('should store result numbers in an array', () => {
+    const testArray = new SudArray([1,2,0,4,7,7,7,8,9,1,2,3,4,5,6,7,8,9,1,2,5,4,5,6,7,8,9],[])
+    expect(testArray.buildSolution()).toEqual([[[2,6]]]);
   });
 })
--- src/main.js
+++ src/main.js
@@ -1,20 +1,34 @@
 //import $ from 'jquery';
-import 'bootstrap';
+//import 'bootstrap';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-export default function SudArray(grid) {
+export default function SudArray(grid,solution) {
   this.grid = grid;
+  this.solution = solution;
 }
 
 const fullArraySudoku = new SudArray([5,3,0,0,7,0,0,0,0,6,0,0,1,9,5,0,0,0,0,9,8,0,0,0,0,6,0,8,0,0,0,6,0,0,0,3,4,0,0,8,0,3,0,0,1,7,0,0,0,2,0,0,0,6,
-  0,6,0,0,0,0,2,8,0,0,0,0,4,1,9,0,0,5,0,0,0,0,8,0,0,7,9]);
+  0,6,0,0,0,0,2,8,0,0,0,0,4,1,9,0,0,5,0,0,0,0,8,0,0,7,9],[]);
+
+let blankPosition = []  
 
 SudArray.prototype.pushIntoArray = function(position) {
     this.grid[5] = 1; 
 };
 
-SudArray.prototype.findNextBlank = function() {
-  let blankPosition = [] 
+SudArray.prototype.buildSolution = function() {
+  blankPosition = this.findBlanks();
+  let x
+  for (let i = 0; i<blankPosition.length; i++) {
+    x = this.findNumberToTry()
+    this.solution.push(x);
+  }
+  return this.solution;
+}
+
+
+SudArray.prototype.findBlanks = function() {
+  blankPosition = []
   for (let i=0; i<81; i++) {
     if (this.grid[i] === 0) {
       blankPosition.push(i);
@@ -24,30 +38,54 @@ SudArray.prototype.findNextBlank = function() {
 };
 
 SudArray.prototype.findNumberToTry = function() {
-  let blankPosition = this.findNextBlank();
-  let numberTrial = [];
-  for(let i = 0; i<blankPosition; i++) {
-    //this.checkRow(i);
+  blankPosition = this.findBlanks();
+  let r = 1;
+  let q;
+  for (let i=r; i<9; i++) {
+    q = this.checkRow(blankPosition[0],r);
+    r = this.checkColumn(blankPosition[0],q);
+    if(i<r) {i=r};
   }
-  return blankPosition;
+  let positionAndTrial = []
+  positionAndTrial.push([blankPosition[0],r])
+  return positionAndTrial
 };
 
-SudArray.prototype.checkRow = function(position) {
-  //let testrow = []
-  let count = 0
-  for (let j = 1; j<10; j++){
-    for (let i=(parseInt(position/9))*9; i<((parseInt(position/9))*9)+9; i++) {
-    //testrow.push(i);
+SudArray.prototype.checkRow = function(position,startNumber) {
+  let answerToCheckRow =[]
+  for (let j = startNumber; j<10; j++){  //tries startNumber-9 in turn to find the lowest missing number
+    let count = 0
+    for (let i=(parseInt(position/9))*9; i<((parseInt(position/9))*9)+9; i++) {  
+      //compares 1-9 against each of the elements in the row
       if (this.grid[i] === j) {
-        count = 1;
+        count = 1;  //if it finds a 1-9 the count set to 1
         }
       }
-    if (count === 0) {
-      return j  
+    if (count === 0) {  // if it hasn't found a 1-9 in all the positions in the 
+      answerToCheckRow.push(j);          //row it returns the 1-9
     }
   };
-  //let rowMinimum = Math.min(...testrow);
-}
+return answerToCheckRow[0]  
+};
+
+SudArray.prototype.checkColumn = function(position,startNumber) {
+  for (let j = startNumber; j<10; j++){  //tries startNumber-9 in turn to find the lowest missing number
+    let count = 0
+    for (let i=(position%9); i<81; i=i+9) {  
+      //compares 1-9 against each of the elements in the column
+      if (this.grid[i] === j) {
+        count = 1;  //if it finds a 1-9 the count set to 1
+        }
+      }
+    if (count === 0) {  // if it hasn't found a 1-9 in all the positions in the 
+      return j          //row it returns the 1-9
+    }
+  };
+};
+
+
+
+
 
 
 
